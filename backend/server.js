da744322ed2cc76abb3aaacef487c870108bb4ab
T095T <pdeshpande540@gmail.com>
2025-12-16 19:39:12 +0530

    fixed user creation

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
-import express, { urlencoded } from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./ingest/index.js"
 dotenv.config();
+import express, { urlencoded } from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./ingest/index.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.json());
-app.use(cors())
-app.use(urlencoded({ extended: true }))
-app.use(clerkMiddleware())
-
+app.use(cors());
+app.use(urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
 //route
-app.get('/',(req,res)=> res.send("Server is Live..."));
+app.get("/", (req, res) => res.send("Server is Live..."));
 //inngest route
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-
-app.listen(port,()=>console.log(`Port running on ${port}`))
+app.listen(port, () => console.log(`Port running on ${port}`));
